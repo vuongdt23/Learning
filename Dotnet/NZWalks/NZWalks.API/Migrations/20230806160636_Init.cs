a13af897cc1d5d4d74aa64bfaab747dfbd98f55d
Duong Thanh Vuong <vuong.dt.23@gmail.com>
2023-08-06 23:45:23 +0700

    ef first migration and db config

---
+++ Dotnet/NZWalks/NZWalks.API/Migrations/20230806160636_Init.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Difficulties",
+                columns: table => new
+                {
+                    DifficultyId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Difficulties", x => x.DifficultyId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    RegionId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    RegionCode = table.Column<string>(type: "longtext", nullable: false),
+                    RegionName = table.Column<string>(type: "longtext", nullable: false),
+                    RegionImgUrl = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.RegionId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    WalkName = table.Column<string>(type: "longtext", nullable: false),
+                    WalkDescription = table.Column<string>(type: "longtext", nullable: false),
+                    WalkImgUrl = table.Column<string>(type: "longtext", nullable: true),
+                    DifficultyId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    RegionId = table.Column<Guid>(type: "char(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Walks_Difficulties_DifficultyId",
+                        column: x => x.DifficultyId,
+                        principalTable: "Difficulties",
+                        principalColumn: "DifficultyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Walks_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "RegionId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_DifficultyId",
+                table: "Walks",
+                column: "DifficultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_RegionId",
+                table: "Walks",
+                column: "RegionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "Difficulties");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
