fef923ad4d198cc91e5fd368300818c2933243a5
thanhvuong.duong <vuong.dt.23@gmail.com>
2023-08-17 14:28:58 +0700

    dapr helloworld project created

---
+++ Dotnet/WithDapr/dapr.microservice.webapi/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+namespace THDapr.dapr.microservice.webapi.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class HelloController : ControllerBase
+{
+    private readonly ILogger<HelloController> _logger;
+    public HelloController(ILogger<HelloController>
+    logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet()]
+    public ActionResult<string> Get()
+    {
+        Console.WriteLine("Hello, World.");
+        return "Hello, World";
+    }
+}
